0206a2b9df4efb094f5092f128c40f6dfa00814d
Juansi93 <93302114+Juansi93@users.noreply.github.com>
2022-03-02 14:53:39 -0300

    Creating routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route } from 'react-router-dom';
+
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>  )
+}
+ 
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+      <Route exact path='/' element={<HomePage />} />
+      <Route path='/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
